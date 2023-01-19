61631d0ea664807ab1b95b907fa7f0c82876dc47
Filippa Fast <filippa.fast@learnet.se>
2023-01-19 10:31:25 +0100

    Installerat Syncfusion

--- C-Sharp-Project/Program.cs
+++ C-Sharp-Project/Program.cs
@@ -2,6 +2,7 @@ using C_Sharp_Project;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorBootstrap;
+using Syncfusion.Blazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -13,4 +14,5 @@ builder.Services.AddScoped(sp =>
     });
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddBlazorBootstrap();
+builder.Services.AddSyncfusionBlazor();
 await builder.Build().RunAsync();
