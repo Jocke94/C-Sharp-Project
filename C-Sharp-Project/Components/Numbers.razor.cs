3e6218b7f03489b179c5bf2f642496e6510fad7a
jocke94 <joakim94larsson@gmail.com>
2023-01-31 14:42:33 +0100

    Compare

--- C-Sharp-Project/Components/Numbers.razor.cs
+++ C-Sharp-Project/Components/Numbers.razor.cs
@@ -12,17 +12,7 @@ namespace C_Sharp_Project.Components
 			//som har två properties 
 			public int MyNumbers { get; set; }
 			public bool IsSelected { get; set; }
-			
-
 		}
-		/*
-		//partial för att kunna deklarera samma klass i olika klasser
-		public partial class Randomize
-		{
-			public int MyRandom { get; set; }
-
-		}
-		*/
 	}
 }
 
