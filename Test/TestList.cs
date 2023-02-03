b692f1791a75d4dc2ee148600a8651498f91460b
jocke94 <joakim94larsson@gmail.com>
2023-02-03 13:57:35 +0100

    tests

---
+++ Test/TestList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+		public class UnitTest1
+		{
+			public class ListItem
+			{
+				//som har två properties 
+				public int MyNumbers { get; set; }
+				public bool IsSelected { get; set; }
+			}
+
+			[Fact]
+			public void TestaLista()
+			{
+				//Arrange
+				var chooseNumbers = Enumerable.Range(1, 70).Select(i => new ListItem { MyNumbers = i }).ToList();
+				chooseNumbers[0].IsSelected = true;
+				chooseNumbers[5].IsSelected = true;
+				var expectedNumbers = new List<int> { 1, 6 };
+				var actualNumbers = new List<int>();
+
+				//Act
+				foreach (var number in chooseNumbers.Where(n => n.IsSelected))
+				{
+					actualNumbers.Add(number.MyNumbers);
+				}
+
+				//Assert
+				Assert.Equal(expectedNumbers, actualNumbers);
+			}
+		}
+}
+
